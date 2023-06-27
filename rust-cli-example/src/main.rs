23c82eb2eb73fd66e6ca69e3755d08bf6a13e9fc
Seth Woinowsky <SethWoinowsky@gmail.com>
2023-06-27 09:34:05 -0700

    Added tabs and set their render to show up as the first part of the displayed 'chunks'

--- rust-cli-example/src/main.rs
+++ rust-cli-example/src/main.rs
@@ -75,6 +75,9 @@ fn main()  -> Result<(), Box<dyn std::error::Error>>  {
     let mut terminal = Terminal::new(backend)?;
     terminal.clear()?;
 
+    let menu_titles = vec!["Home", "Pets", "Add", "Delete", "Quit"];
+    let mut active_menu_item = MenuItem::Home;
+
     loop {
         terminal.draw(|rect| {
             let size = rect.size();
@@ -103,5 +106,30 @@ fn main()  -> Result<(), Box<dyn std::error::Error>>  {
                 );
 
             rect.render_widget(copyright, chunks[2]);
-            
+
+            let menu = menu_titles
+                .iter()
+                .map(|t| {
+                    let (first, rest) = t.split_at(1);
+                    Spans::from(vec![
+                        Span::styled(
+                            first,
+                            Style::default()
+                                .fg(Color::Yellow)
+                                .add_modifier(Modifier::UNDERLINED),
+                        ),
+                        Span::styled(rest, Style::default().fg(Color::White)),
+                    ])
+                })
+                .collect();
+
+            let tabs = Tabs::new(menu)
+                .select(active_menu_item.into())
+                .block(Block::default().title("Menu").borders(Borders::ALL))
+                .style(Style::default().fg(Color::White))
+                .highlight_style(Style::default().fg(Color::Yellow))
+                .divider(Span::raw("|"));
+
+            rect.render_widget(tabs, chunks[0]);
+
 }
