16d2b2e421a39a36cba2689025628351d50f5d1d
Seth Woinowsky <SethWoinowsky@gmail.com>
2023-06-27 08:35:49 -0700

    Added menuItem enum, set terminal to Raw mode, and created mpsc channel per tutorial.

--- rust-cli-example/src/main.rs
+++ rust-cli-example/src/main.rs
@@ -1,7 +1,8 @@
 
-use std::io;
+use std::{io, sync::mpsc, thread, time::{Instant, Duration}};
 use chrono::{DateTime, Utc};
 use serde::{Serialize, Deserialize};
+use crossterm::{terminal, event};
 use thiserror::Error;
 
 const DB_PATH: &str = "./data/db.json";
@@ -28,6 +29,44 @@ enum Event<I> {
     Tick,
 }
 
-fn main() {
-    println!("Hello, world!");
+enum MenuItem {
+    Home,
+    Pets,
+}
+
+impl From<MenuItem> for usize {
+    fn from(input: MenuItem) -> usize {
+        match input {
+            MenuItem::Home => 0,
+            MenuItem::Pets => 1,
+        }
+    }
+}
+
+fn main()  -> Result<(), Box<dyn std::error::Error>>  {
+    terminal::enable_raw_mode().expect("can run in raw mode");
+
+    let (tx, rx) = mpsc::channel();
+    let tick_rate = Duration::from_millis(200);
+    thread::spawn(move || {
+        let mut last_tick = Instant::now();
+        loop {
+            let timeout = tick_rate
+                .checked_sub(last_tick.elapsed())
+                .unwrap_or_else(|| Duration::from_secs(0));
+
+            if event::poll(timeout).expect("poll works") {
+                if let event::Event::Key(key) = event::read().expect("can read events") {
+                    tx.send(Event::Input(key)).expect("can send events");
+                }
+            }
+
+            if last_tick.elapsed() >= tick_rate {
+                if let Ok(_) = tx.send(Event::Tick) {
+                    last_tick = Instant::now();
+                }
+            }
+        }
+    });
+
 }
