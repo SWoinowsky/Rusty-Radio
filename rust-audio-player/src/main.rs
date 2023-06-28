8afa92d65c11ac9f44b40712dd4833ea7e115af7
Seth Woinowsky <Sethwoinowsky@gmail.com>
2023-06-28 15:24:08 -0700

    Now displaying information about detected song and playing for whole duration. Next is onto playing songs in a row.

--- rust-audio-player/src/main.rs
+++ rust-audio-player/src/main.rs
@@ -13,11 +13,24 @@ fn main() {
     let file = BufReader::new(File::open(&path).unwrap());
     let duration = mp3_duration::from_path(&path).unwrap();
 
-    println!("File duration: {:?}", duration);
+    let metadata = mp3_metadata::read_from_file(&path).unwrap();
+
+    println!("File duration: {:?}", metadata.duration);
+    
+    if let Some(tag) = metadata.tag {
+        println!("title: {}", tag.title);
+        println!("artist: {}", tag.artist);
+        println!("album: {}", tag.album);
+        println!("year: {}", tag.year);
+        println!("comment: {}", tag.comment);
+        println!("genre: {:?}", tag.genre);
+    } else {
+        println!("No tag");
+    }
 
     let source = Decoder::new(file).unwrap();
 
     stream_handle.play_raw(source.convert_samples());
 
-    std::thread::sleep(duration);
+    std::thread::sleep(metadata.duration);
 }
